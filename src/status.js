1bfb67beb5f2688aa4d86f991c08614a6bfc660e
Russell Garner <rgarner@zephyros-systems.co.uk>
2023-11-18 11:47:20 +0000

    Move debounce function to own module
    Status is using it, but autocomplete will be in a moment.

--- src/status.js
+++ src/status.js
@@ -1,20 +1,6 @@
 import { createElement, Component } from 'preact' /** @jsx createElement */
+import { debounce } from './debounce'
 
-const debounce = function (func, wait, immediate) {
-  var timeout
-  return function () {
-    var context = this
-    var args = arguments
-    var later = function () {
-      timeout = null
-      if (!immediate) func.apply(context, args)
-    }
-    var callNow = immediate && !timeout
-    clearTimeout(timeout)
-    timeout = setTimeout(later, wait)
-    if (callNow) func.apply(context, args)
-  }
-}
 const statusDebounceMillis = 1400
 
 export default class Status extends Component {
